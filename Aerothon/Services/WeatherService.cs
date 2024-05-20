3b697b745cf4c034c0ff980d98e9e3fdda6c8d64
Narayan soni <narayansoni854@gmail.com>
2024-05-20 21:29:38 +0530

    add weather prediction

---
+++ Aerothon/Services/WeatherService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using WeatherApi2._0.Model;
+using WeatherApi2._0.Services.Interface;
+
+namespace WeatherApi2._0.Services
+{
+    public class WeatherService : IWeatherService
+    {
+        private InferenceSession _session;
+
+        public WeatherService(InferenceSession session)
+        {
+            _session = session;
+        }
+
+        public string getWeatherPrediction(WeatherParams data)
+        {
+            var input = new DenseTensor<float>(
+                new[]
+                {
+                    (float)data.Rain,
+                    data.Temperature,
+                    data.ApparentTemperature,
+                    data.Humidity,
+                    data.WindSpeed,
+                    data.WindBearing,
+                    data.Visibility,
+                    data.Pressure
+                },
+                new[] { 1, 8 }
+            );
+
+            // Create a list of NamedOnnxValue for input
+            var inputs = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", input)
+            };
+
+            // Run the model
+            using var results = _session.Run(inputs);
+
+            // Get the prediction result from the first item in the results collection
+            var prediction = results[0].Value;
+
+            // Return the prediction result
+            var denseTensor = (DenseTensor<string>)prediction; // Cast prediction to DenseTensor<string>
+
+            // Extract the string values from the tensor and convert them to a list
+            var predictedList = denseTensor.ToArray().ToList()[0];
+
+            return predictedList;
+        }
+    }
+}
--- Aerothon/Startup.cs
+++ Aerothon/Startup.cs
@@ -2,6 +2,10 @@
 using Aerothon.Repository.Interfaces;
 using Aerothon.Services;
 using Aerothon.Services.Interfaces;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.OpenApi.Models;
+using WeatherApi2._0.Services;
+using WeatherApi2._0.Services.Interface;
 
 namespace IMDB
 {
@@ -18,8 +22,19 @@ namespace IMDB
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddHttpClient();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WeatherApi2._0", Version = "v1" });
+            });
             services.AddSingleton<IFlightRepository, FlightRepository>();
             services.AddSingleton<IFlightService, FlightService>();
+
+            services.AddSingleton<InferenceSession>(
+                new InferenceSession("MLModel/weather_safety_model.onnx")
+            );
+            services.AddSingleton<IWeatherService, WeatherService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,6 +43,10 @@ namespace IMDB
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "WeatherApi2._0 v1")
+                );
             }
 
             app.UseHttpsRedirection();
