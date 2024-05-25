7dd80b1bea339bea9af6969f55d9cd235e717ce5
Narayan soni <narayansoni854@gmail.com>
2024-05-25 11:22:53 +0530

    fix spelling mistake

--- Aerothon/Helper/WayPointHelper.cs
+++ Aerothon/Helper/WayPointHelper.cs
@@ -32,15 +32,15 @@ namespace Aerothon.Helper
             const double R = 6371; // Radius of the Earth in kilometers
             var lat1 = ToRadians(Latitude);
             var lon1 = ToRadians(Longitude);
-            var lat2 = ToRadians(other.Lattitude);
+            var lat2 = ToRadians(other.Latitude);
             var lon2 = ToRadians(other.Longitude);
 
             var dlat = lat2 - lat1;
             var dlon = lon2 - lon1;
 
-            var a = Math.Sin(dlat / 2) * Math.Sin(dlat / 2) +
-                    Math.Cos(lat1) * Math.Cos(lat2) *
-                    Math.Sin(dlon / 2) * Math.Sin(dlon / 2);
+            var a =
+                Math.Sin(dlat / 2) * Math.Sin(dlat / 2)
+                + Math.Cos(lat1) * Math.Cos(lat2) * Math.Sin(dlon / 2) * Math.Sin(dlon / 2);
             var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
             var distance = R * c;
 
